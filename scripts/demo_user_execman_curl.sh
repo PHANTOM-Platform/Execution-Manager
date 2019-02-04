ea0f5a040bef9a301c5e0354933083d30d3ebde6
Jose Miguel Montanana <jmmontanana@gmail.com>
2019-02-04 11:44:40 +0100

    Add files via upload

--- scripts/demo_user_execman_curl.sh
+++ scripts/demo_user_execman_curl.sh
@@ -9,7 +9,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#  http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -21,7 +21,7 @@
 # Please replace such spaces by %20
 #0. ########## GLOBAL VARIABLES ###################################
 	BASE_DIR=`pwd`;
-	server="localhost"; 
+	server="localhost";
 	execmanager_port="8700";
 	app=`basename $0`;
 	BLUE="\033[0;34m";
@@ -43,16 +43,16 @@
 	cd `dirname $0`;
 #0. #### Function Scripts definition ################################
 	verify_reponse()
-	{ 
+	{
 		# $1 server
 		# $2 port
 		echo "Checking Response on port ${2} ...";
-		let "j=0"; 
+		let "j=0";
 		HTTP_STATUS=$(curl --silent --output /dev/null --write-out "%{http_code}" http://${1}:${2});
-		while [[ ${HTTP_STATUS} != "200" ]] && [ ${j} -lt 1 ] ; do 
-			let "j += 1 "; sleep 1; 
+		while [[ ${HTTP_STATUS} != "200" ]] && [ ${j} -lt 1 ] ; do
+			let "j += 1 "; sleep 1;
 			HTTP_STATUS=$(curl --silent --output /dev/null --write-out "%{http_code}" http://${1}:${2});
-		done; 
+		done;
 		if [[ ${HTTP_STATUS} != "200" ]]; then
 			echo "> Server is unreachable on port ${2}. Aborting."
 			exit 1;
@@ -93,7 +93,7 @@
 	curl -s -H "Authorization: OAuth ${mytoken}" -H "Content-Type: multipart/form-data" -XPOST -F "UploadJSON=@../web-execmanager/execstatus.json" http://${server}:${execmanager_port}/register_new_exec;
 	#We sync, because it may start the next command before this operation completes.
 	curl -s -XGET ${server}:${execmanager_port}/_flush > /dev/null;
-# 12. ################## TEST OF UPDATE a execution ######## 
+# 12. ################## TEST OF UPDATE a execution ########
 	echo -e "\n${LIGHT_BLUE}";
 	echo "curl -s -H \"Authorization: OAuth \${mytoken}\" -H \"Content-Type: multipart/form-data\" -XPOST -F \"UploadJSON=@../web-execmanager/execstatus_mod.json\" http://${server}:${execmanager_port}/update_exec;";
 	read -p $'Press [Enter] key to update_exec'; echo -e "${NO_COLOUR}";
@@ -144,28 +144,26 @@
 	echo "curl -s -H \"Authorization: OAuth ${mytoken}\" -H \"Content-Type: multipart/form-data\" -XGET http://${server}:${execmanager_port}/get_user_defined_metrics?appid=\"demo\"\\&taskid=\"pthread-example\"";
 	read -p $'Press [Enter] key to \033[1;37mRUN THE REQUEST\033[1;34m with \033[1;37mVALID TOKEN\033[1;34m'; echo -e "${NO_COLOUR}" ;
 	curl -s -H "Authorization: OAuth ${mytoken}" -H "Content-Type: multipart/form-data" -XGET http://${server}:${execmanager_port}/get_user_defined_metrics?appid="demo"\&taskid="pthread-example"\&experimentid="AWU87m3quRkaX6JeceE9"
-	
+
 if [ -e token.txt ]; then rm token.txt; fi;
 exit
-
 
 Query:
 curl -XGET 'localhost:9400/mf/experiments/_search?size=100&pretty="true"' -d '{"query": { "term": { "_id": "AWU87m3quRkaX6JeceE9" }}}'
 
 update (it is merging):
-curl -XPOST localhost:9400/mf/experiments/AWU87m3quRkaX6JeceE9/_update?routing="demo" -d '{               
-       "doc":{   "domain" : "domain_gmv" } }'
-
+curl -XPOST localhost:9400/mf/experiments/AWU87m3quRkaX6JeceE9/_update?routing="demo" -d '{
+"doc":{ "domain" : "domain_gmv" } }'
 }, {
-      "_index" : "mf",
-      "_type" : "experiments",
-      "_id" : "AWU87m3quRkaX6JeceE9",
-      "_score" : 1.0,
-      "_routing" : "demo",
-      "_parent" : "demo",
-      "_source" : {
-        "application" : "demo",
-        "task" : "pthread-example",
-        "host" : "node01",
-        "@timestamp" : "2018-08-15T11:33:03.330"
-      }
+	"_index" : "mf",
+	"_type" : "experiments",
+	"_id" : "AWU87m3quRkaX6JeceE9",
+	"_score" : 1.0,
+	"_routing" : "demo",
+	"_parent" : "demo",
+	"_source" : {
+		"application" : "demo",
+		"task" : "pthread-example",
+		"host" : "node01",
+		"@timestamp" : "2018-08-15T11:33:03.330"
+	}
